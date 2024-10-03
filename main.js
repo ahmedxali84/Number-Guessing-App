bc36761973b4beff19a44ff89a750eb77d733ac5
ahmedxali84 <universalme.mss@gmail.com>
2024-10-04 01:20:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+   document.getElementById("button");
+   var number = prompt("Enter Your Number :");
+   if(number == 60){
+    alert("Your  Answer Is Correct!!!!")
+   }
+   else if(number>60){
+    alert("Too High!!!")
+   }
+   else if(number<60){
+    alert("Too Loww!!!")
+   }
+}
