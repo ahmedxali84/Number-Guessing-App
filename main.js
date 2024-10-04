a3fbf0eff89526e1da6c52cda2cc1c81bbcab1a4
ahmedxali84 <universalme.mss@gmail.com>
2024-10-04 14:46:52 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+var audio = new audio("./song.mp3");
 function myFunction() {
    document.getElementById("button");
    var number = prompt("Enter Your Number :");
    if(number == 60){
+      audio.play();
     alert("Your  Answer Is Correct!!!!")
    }
    else if(number>60){
